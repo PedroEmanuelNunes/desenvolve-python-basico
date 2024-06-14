d1b78f022c598d0e256c982b98f2eb029bbfb391
PedroEmanuelNunes <pedroemanuelsilvanunes@gmail.com>
2024-06-13 21:17:22 -0300

    2.2 - Tipos de dados

---
+++ modulo2/aula2_questão2.py
@@ -0,0 +1,9 @@
+print("O resultado é")
+v1 = 10
+v2 = 3.5
+
+soma = v1 + v2
+print(soma)
+print(type(v1))
+print(type(v2))
+print(type(soma))
---
+++ modulo2/aula2_questão3.py
@@ -0,0 +1,8 @@
+v1 = 10
+v2 = 20
+
+troca = v1
+v1 = v2
+v2 = troca
+print("Novo valor de v1:", v1)
+print("Novo valor de v2:", v2)
